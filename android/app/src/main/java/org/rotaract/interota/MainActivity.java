acbb38804c4c164da98eebc2a8e78c0cd41eab3a
Labib Mezghanni <mezghanni.labib@gmail.com>
2023-02-20 21:42:40 +0100

    add android and ios

--- android/app/src/main/java/org/rotaract/interota/MainActivity.java
+++ android/app/src/main/java/org/rotaract/interota/MainActivity.java
@@ -1,13 +1,5 @@
 package org.rotaract.interota;
 
-import android.util.Log;
-
-import androidx.annotation.NonNull;
-
 import com.getcapacitor.BridgeActivity;
-import com.google.android.play.core.tasks.Task;
 
-public class MainActivity extends BridgeActivity {
-
-
-}
+public class MainActivity extends BridgeActivity {}
